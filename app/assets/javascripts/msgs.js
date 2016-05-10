596b17defd99f69ffe15347b040a61c9fd92872c
Damian Kosowski <kosowski.d@gmail.com>
2016-05-10 13:20:48 +0200

    Added active class functionality

--- app/assets/javascripts/msgs.js
+++ app/assets/javascripts/msgs.js
@@ -1,60 +1,79 @@
 $(document).ready( function() {
 
-	function parseMsg(response) {
+	function createMsgTemplate(message) {
 		var html = "<div class='message'>" 
-						+ "<a rel='no-follow' data-method='delete' href='/msgs/" + response.id + "'>"
+						+ "<a rel='no-follow' data-method='delete' href='/msgs/" + message.id + "'>"
 						+ "<span class='delete-message-button'>&#10006;</span></a>"
-						+ response.send_time
+						+ message.send_time
 						+ "| <strong>"
-						+ response.sender.name
+						+ message.sender.name
 						+ ":</strong> "
-						+ "<span class='message-text'>" + response.content + "</span>"
+						+ "<span class='message-text'></span>"
 						+ "</div>";
 		return html	
 	}
 
-	function scrollView() {
-		document.body.scrollTop = $('body').height();
-	}
-
-	scrollView();
-	
-	$('[href]').on("ajax:success", function(event, response) {
-		$('.top-menu').hide()
-		$('.message-container').html(response).hide().fadeIn();
-		scrollView();
-		setTimeout(function () { $('.top-menu').fadeIn() }, 200);
-		window.history.replaceState("", "", $(this).attr("href"));
-		$('#search-form').attr("action", $(this).attr("href"));
-		hrefArray = window.location.pathname.split('/');
-		console.log(hrefArray);
-		$('#msg_recipient_type').val(hrefArray[2].charAt(0).toUpperCase() + hrefArray[2].slice(1));
-		$('#msg_recipient_id').val(hrefArray[3]);
-	});
-
-	$('#search-form').on("ajax:success", function(even, response) {
-		$('.message-container').html(response).hide().fadeIn();
-		var searchedText = $('#search-form input[type="text"]').val();
-		$('.message-text').each( function () {
-			var messageText = $(this).html()
-			var matchPhrase = new RegExp(searchedText, "gi")
-			var matchedText = messageText.match(matchPhrase)[0]
-			var foundText = messageText.replace(matchPhrase , "<span class='searched-text'>" + matchedText + "</span>");
-			$(this).html(foundText);
-		});
-		scrollView();
-		setTimeout(function () { $('.top-menu').fadeIn() }, 250);
-	});
-
 	$('#new_msg').on("ajax:success", function(event, response) {
-		var msg = parseMsg(response);
-		$('.message-content').append(msg);
+		var msgTemplate = createMsgTemplate(response);
+		$('.message-content').append(msgTemplate);
+		$('.message:last-child .message-text').text(response.content);
 		$('.message:last-child').hide().slideDown(200);
 		$('#new_msg input[type="text"]').val("");
 		$('body').animate({ scrollTop: $('body').height() }, 800);
 	});
 
-// Automatic msgs pulling (showing active channels and users)
+	function scrollView() {
+		$('.top-menu').hide();
+		$('.message-container').hide().fadeIn();
+		document.body.scrollTop = $('body').height();
+		setTimeout(function () { $('.top-menu').fadeIn() }, 200);
+	}
+
+	function toggleActiveLinks(element) {
+		if ( element.parent().attr('role') === 'menuitem') {
+			$('[role="menuitem"]').removeClass('active');
+			element.parent().addClass('active');
+		} else {
+			$('li').removeClass('active-link');
+			$('li').removeClass('active');
+			element.children().first().addClass('active-link');
+		}	
+	}
+	
+	$('[href]').on("ajax:success", function(event, response) {
+		var newPath = $(this).attr("href");
+		window.history.replaceState("", "", newPath);
+		var pathArray = window.location.pathname.split('/');
+		
+		$('#search-form').attr("action", newPath);	
+		$('#msg_recipient_type').val(pathArray[2].charAt(0).toUpperCase() + pathArray[2].slice(1));
+		$('#msg_recipient_id').val(pathArray[3]);
+
+		toggleActiveLinks($(this));
+
+		$('.message-container').html(response);
+		scrollView();
+	});
+
+	$('#search-form').on("ajax:success", function(event, response) {
+		var searchedText = $('#search-form input[type="text"]').val();
+
+		$('.message-container').html(response)
+
+		$('.message-text').each( function () {
+			var messageText = $(this).text()
+			var matchPhrase = new RegExp(searchedText, "gi")
+			var matchedText = messageText.match(matchPhrase)[0]
+			var foundText = messageText.replace(matchPhrase , "<span class='searched-text'>" + matchedText + "</span>");
+			$(this).html(foundText);
+		});	
+		scrollView();
+	});
+
+	$('.channel').first().addClass('active-link');
+	$('#refresh-link').hide();
+	scrollView();
+
 
 });
  
